5cb3e548a06fb5b14bf053a4c9525106fab755c1
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-08-20 10:31:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import cv2
 import time
 from ultralytics import YOLO
+import os
 
-model = YOLO("best.pt")
+home_directory = os.path.expanduser('D:/Download/perkuliahan/yolo/Floater ocean and river garbage/')
+model_path = os.path.join(home_directory, 'best.pt')
+video_path = os.path.join(home_directory, 'video.mp4')
+result_path = os.path.join(home_directory, 'Result', 'Result.txt')
+final_result_path = os.path.join(home_directory, 'Result', 'Tresult.txt')
 
-video_path = "./video.mp4"  
+model = YOLO(model_path)
+
 cap = cv2.VideoCapture(video_path)
 
 if not cap.isOpened():
@@ -12,15 +18,19 @@ if not cap.isOpened():
     exit()
 
 frame_count = 0
-deteksi_interval = 3  # interval detection
+deteksi_interval = 3  # Interval detection in seconds
 fps = int(cap.get(cv2.CAP_PROP_FPS))
-deteksi_txt = open("result.txt", "w")  # file output
-deteksi_txt.write("interval {} sec:\n".format(deteksi_interval))
+deteksi_txt = open(result_path, "w")
+deteksi_txt.write("Interval {} seconds:\n".format(deteksi_interval))
+
+total_counts = {"Glass": 0, "Metal": 0, "Plastic": 0}
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+    frame = cv2.resize(frame, (1920, 1080))
 
     frame_count += 1
     current_time = frame_count / fps
@@ -43,10 +53,15 @@ while cap.isOpened():
         deteksi_txt.write("Time: {}m:{}s: ".format(minutes, seconds))
         for cls_name, count in class_counts.items():
             deteksi_txt.write("[{}: {}] ".format(cls_name, count))
+            if cls_name in total_counts:
+                total_counts[cls_name] += count
         deteksi_txt.write("\n")
 
-# Release resources
 cap.release()
 deteksi_txt.close()
 
-print("Detections ends, result in result.txt")
+with open(final_result_path, "w") as final_result_txt:
+    for cls_name, total in total_counts.items():
+        final_result_txt.write("{}: {}\n".format(cls_name, total))
+
+print("Detections finished. Results are in result.txt and Tresult.txt")
