3e80c9e3cf8c8a3928b2d2f473ef59f0f8dbc65b
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-11-25 01:06:37 +0700

    Update path.py

--- path.py
+++ path.py
@@ -3,7 +3,7 @@ import os
 rgd = os.path.dirname(os.path.abspath(__file__))
 
 main
-model_path = os.path.join(rgd, 'Model', 'model.pt')
+model_path = os.path.join(rgd, 'Model', 'rgd.pt')
 video_path = os.path.join(rgd, 'Source', 'video2.mp4')
 result_path = os.path.join(rgd, 'Result', 'Result.txt')
 final_result_path = os.path.join(rgd, 'Result', 'Tresult.txt')
@@ -17,4 +17,4 @@ output_dir = os.path.join(rgd, 'Result', 'Capture')
 box
 image_path = os.path.join(rgd,"Result", "Capture")
 image_out = os.path.join(rgd,"Result", "Capture")
-model_path = os.path.join(rgd, 'Model', 'model.pt')
+model_path = os.path.join(rgd, 'Model', 'rgd.pt')
