c1b0bf549bd9a96da1786551530a152ae4883100
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-12-05 23:13:56 +0700

    Update path.py

--- path.py
+++ path.py
@@ -2,19 +2,19 @@ import os
 
 rgd = os.path.dirname(os.path.abspath(__file__))
 
-main
+#main
 model_path = os.path.join(rgd, 'Model', 'rgd.pt')
 video_path = os.path.join(rgd, 'Source', 'video2.mp4')
 result_path = os.path.join(rgd, 'Result', 'Result.txt')
 final_result_path = os.path.join(rgd, 'Result', 'Tresult.txt')
 class_path = os.path.join(rgd, 'class_names.py')
 
-frame
+#frame
 r_path = os.path.join(rgd, "Result", "Representative.txt")
 video_path = os.path.join(rgd, 'Source', 'video2.mp4')
 output_dir = os.path.join(rgd, 'Result', 'Capture')
 
-box
+#box
 image_path = os.path.join(rgd,"Result", "Capture")
 image_out = os.path.join(rgd,"Result", "Capture")
 model_path = os.path.join(rgd, 'Model', 'rgd.pt')
